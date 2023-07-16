70e1563d5ce2ef747390bce6e6d7191ab3822e6a
Mihaela Catalina <c.mihaela.stoian@gmail.com>
2023-07-16 12:02:28 +0300

    updated links for downloading kinetics weights

--- kinetics-pt/get_kinetics_weights.sh
+++ kinetics-pt/get_kinetics_weights.sh
@@ -3,5 +3,15 @@ wget --load-cookies cookies.txt "https://docs.google.com/uc?export=download&conf
 rm cookies.txt
 wget --load-cookies cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1XBMs4TLt2H378M_a0k23l8let-Ae2AlB' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1XBMs4TLt2H378M_a0k23l8let-Ae2AlB" -O resnet50I3D-NL.pth
 rm cookies.txt
+wget --load-cookies cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1psXLeYkrZYhqqCOGDym8XGBrRG8XyDHs' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1psXLeYkrZYhqqCOGDym8XGBrRG8XyDHs" -O resnet50RCN.pth
+rm cookies.txt
 wget --load-cookies cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1ZpbvJzvnDxJmKCFTs9wKmmA2qvm2aFBX' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1ZpbvJzvnDxJmKCFTs9wKmmA2qvm2aFBX" -O resnet50I3D.pth
+rm cookies.txt
+wget --load-cookies cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1kHSu5PDd3LxkOFDL6dBGsxRySi7GVasi' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1kHSu5PDd3LxkOFDL6dBGsxRySi7GVasi" -O resnet50RCLSTM.pth
+rm cookies.txt
+wget --load-cookies cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1YgeXZk45V7F2chSyRK-uNWrBF5BskdVj' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1YgeXZk45V7F2chSyRK-uNWrBF5BskdVj" -O resnet50RCGRU.pth
+rm cookies.txt
+wget --load-cookies cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1kQO_dnM9JjV3sBtvowXqQa6d0xcxn5rs' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1kQO_dnM9JjV3sBtvowXqQa6d0xcxn5rs" -O SLOWFAST_R50_K400.pth.tar
+rm cookies.txt
+wget --load-cookies cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1qDdAntE5Onh7btniftOL8MrbsD7OIqj4' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1qDdAntE5Onh7btniftOL8MrbsD7OIqj4" -O SLOWFAST_R101_K700.pth.tar
 rm cookies.txt
