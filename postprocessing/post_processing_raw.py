bbe67b84d4268b33638cea0fb32a56580f3e4b86
Mihaela Catalina <c.mihaela.stoian@gmail.com>
2023-10-05 19:08:01 +0200

    added postprocessing script (input consists of raw neural network scores, one score per label)

--- postprocessing/post_processing_raw.py
+++ postprocessing/post_processing_raw.py
@@ -1,11 +1,12 @@
 ############
-# POST-PROCESS YOUR PKL FILE TO MAKE IT COMPLIANT TO THE REQUIREMENTS
+# POST-PROCESS YOUR PKL FILE TO MAKE IT COMPLIANT TO THE REQUIREMENTS. THIS VERSION TAKES INTO ACCOUNT THE CONFIDENCE OF THE MODEL FOR EACH LABEL BEFORE POST-PROCESSING THE OUTPUT. 
+# IMPORTANT NOTE: IT TAKES AS INPUT A FILE IN THE SAME FORMAT AS THE ONE REQUIRED FOR TASK 1. IT OUTPUTS A FILE COMPLIANT WITH TASK 2
 # INPUTS NECESSARY FOR MAIN:
 #  --file_path : path to your pkl file
 #  --requirements_path : path to the requirements in WDIMACS format
+#  --threshold: threshold at which a label is considered predicted
 ############
 # Based on https://github.com/EGiunchiglia/ROAD-R/blob/main/post_processing.py
-
 
 import glob
 import zipfile
@@ -18,26 +19,12 @@ import subprocess
 NUM_LABELS = 41
 
 
-###############
-# Take a prediction and return its negated
-###############
-def invert_pred(pred, th):
-    # Since the predictions that get changed are not given by the model, we assign to them the lowest confidence possible (i.e., threhsold plus/minus epsilon)
-    epsilon = 1e-3
-    if pred == th:
-        return th
-    elif pred < th:
-        new_pred = th + epsilon
-    else:
-        new_pred = th - epsilon
-    return new_pred
-
 
 ###########
 # Write temporary file with requirements (hard constraints) and output (soft constraints)
 # Note: hard constraints have wegith 50.0, soft constraints have weight depending on the confidence of the model in the prediction
 ###########
-def write_temp_file(preds, temp_file_path, wdimacs_path):
+def write_temp_file(pred, temp_file_path, wdimacs_path, th):
 
     with open(wdimacs_path, 'r') as f:
         with open(temp_file_path, 'w') as wf:
@@ -45,11 +32,11 @@ def write_temp_file(preds, temp_file_path, wdimacs_path):
             for i, line in enumerate(f.readlines()):
                 wf.write(line)
             # Write the soft constraints (i.e., the predictions)
-            for i in range(NUM_LABELS): 
-                if i in preds:
-                    wf.write(f"1 {i+1} 0\n")
+            for i, p in enumerate(pred): 
+                if p > th:
+                    wf.write(f"{p} {i+1} 0\n")
                 else:
-                    wf.write(f"1 -{i+1} 0\n")
+                    wf.write(f"{1-p} -{i+1} 0\n")
 
 
 ###########
@@ -92,6 +79,7 @@ def main():
     parser = argparse.ArgumentParser(description='Post-processing output')
     parser.add_argument('--file_path', type=str, help='Path to the output file to post-process')
     parser.add_argument('--requirements_path', type=str, default='WDIMACS_requirements.txt', help='Path to the requirements file in WDIMACS format')
+    parser.add_argument('--threshold', type=float, default=0.5, help='Threshold at which a label is considered as predicted. If label > threshold then it is considered predicted.')
     args = parser.parse_args()
     
     # Set the file path for the temp req file so that we can write the temporary file in the same location as the requirements file
@@ -103,27 +91,29 @@ def main():
     with open(file, 'rb') as fff:
         preds = pickle.load(fff)
         del fff
-        
-    # preds = {'video1':{'image1': [{'bbox': [], 'labels': [16,37]}, {'bbox': [], 'labels': [10]}]}, 
-    #          'video2':{'image1': [{'bbox': [], 'labels': [1,37]}, {'bbox': [], 'labels': [2, 21,0]}]}}
     
     for videoname in preds.keys(): 
         print(videoname)
         preds_framenames = preds[videoname].keys()
         for pred_framename in preds_framenames: 
             
+            new_bboxes = []
             for i, bbox in enumerate(preds[videoname][pred_framename]):
                 pred_labels = bbox['labels']
                 
+                # We cannot correct boxes for which no labels are predicted
+                if all([p < args.threshold for p in pred_labels]):
+                    continue
                 # Write temporary file containing hard and soft constraints
-                write_temp_file(pred_labels, requirements_temp_path, args.requirements_path)
+                write_temp_file(pred_labels, requirements_temp_path, args.requirements_path, args.threshold)
                 # call the solver on the temporary file
                 cost, new_assgn, time_elaps = call_solver(requirements_temp_path)            
                 # compute the new predictions                
                 #compute the new labels to attach to the bbox
-                final_pred_labels = [int(l)-1 for l in new_assgn if '-' not in l]
+                final_pred_labels = [int(l)-1 for l in new_assgn if '-' not in l] 
 
-                preds[videoname][pred_framename][i]['labels'] = final_pred_labels  
+                new_bboxes.append({'bbox': preds[videoname][pred_framename][i]['bbox'], 'labels':final_pred_labels})
+            preds[videoname][pred_framename] = new_bboxes  
 
     with open(f"post_processed_{args.file_path}", 'wb') as handle:
         pickle.dump(preds, handle)
